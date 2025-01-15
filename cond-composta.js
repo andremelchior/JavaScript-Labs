92772f86071e95b7c5e6ee2ff455b7f84a3a6ef8
André Melchior <andre.melchior@outlook.com>
2025-01-15 20:13:05 -0300

    feat: add JS and HTML files.

---
+++ cond-composta.js
@@ -0,0 +1,9 @@
+// condição composta
+
+var pais = 'BR'
+
+if (pais == 'BR'){
+    console.log("brasileiro");
+} else {
+    console.log("estrangeiro");
+}
---
+++ cond-simples.js
@@ -0,0 +1,8 @@
+// condição simples
+
+var velocidade = 12
+console.log("A velocidade do seu carro é: " + velocidade);
+
+if (velocidade > 60){
+    console.log("ultrapassou a velocidade permitida");
+}
