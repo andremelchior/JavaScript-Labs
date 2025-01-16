c8956d364273c577e2f61e30a5dc3121296e52fc
André Melchior <andre.melchior@outlook.com>
2025-01-16 08:48:56 -0300

    feat: add arithmetic operations.

---
+++ OpAritmeticos.js
@@ -0,0 +1,27 @@
+//Operadores Aritméticos
+
+10 + 10; //adição
+10 - 5; //subtração
+10 * 5; //multiplicação
+5 / 10; //divisão
+2 ** 2; //potenciação
+4 % 2; //resto
+
+var x = 10,
+    y = 5;
+var result = x + y;
+
+x += 1; //incremento  x = x + 1;
+
+//x -= 1; decremento  x = x + 1;
+console.log(x);
+console.log(result);
+
+let a = 5;
+a++; //incremento = 6
+
+let b = 5;
+b--; //decremento = 4
+
+console.log(a);
+console.log(b);
