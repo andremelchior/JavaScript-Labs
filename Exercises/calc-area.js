04d7db264c7916fab7b27b4d84629de33217ca4a
André Melchior <andre.melchior@outlook.com>
2025-01-15 21:35:21 -0300

    feat: add circle area calculation with predefined radius and PI.

---
+++ Exercises/calc-area.js
@@ -0,0 +1,10 @@
+// Faça um algoritmo para calcular a área de uma circunferência, considerando a fórmula ÁREA = PI * RAIO². Utilize as variáveis AREA e RAIO, a constante PI(pi = 3,14159) e os operadores aritméticos de multiplicação
+
+let area;
+let raio = 7.5;
+
+const PI = 3.1415;
+
+area = PI * raio ** 2;
+
+console.log("A área calculada é: " + area);
