4a78590f686610d19e2c4a74144236640ac188a1
André Melchior <andre.melchior@outlook.com>
2025-01-15 21:30:35 -0300

    feat: add calculation of double and triple of a number.

---
+++ Exercises/dobro-triplo.js
@@ -0,0 +1,9 @@
+//Faça um algoritmo que obtenha como resultado o dobro e o triplo de um número armazenado na memória e exiba os resultados.
+
+let num = 2;
+let dobro, triplo;
+
+dobro = num * 2;
+triplo = num * 3;
+
+console.log("O dobro deste valor é: " + dobro + ". O triplo é: " + triplo);
