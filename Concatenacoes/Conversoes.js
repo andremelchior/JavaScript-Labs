fe0102936295020bb939a6058c16a81c872d79c0
André Melchior <andre.melchior@outlook.com>
2025-01-18 14:02:21 -0300

    docs: add brief comment about 'Interpolation'.

--- Concatenacoes/Conversoes.js
+++ Concatenacoes/Conversoes.js
@@ -5,3 +5,5 @@ var n2 = parseInt(window.prompt("Digite outro numero inteiro:")); //string para 
 var soma = n1 + n2;
 window.alert(`A soma de ${n1} e ${n2} é igual a: ${soma}`);
 // O sinal de + pode servir tanto para soma como concatenação, porém "$" só funciona como concatenação se tiver entre crases "``".
+
+//Esse tipo de concatenação se chama "Interpolation" que vem através de templete strings.
