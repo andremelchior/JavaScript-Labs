f5133f4fbb2d1ee90ab5c8efc1bd5ed931e8b7d6
André Melchior <andre.melchior@outlook.com>
2025-01-18 17:10:32 -0300

    feat: add loopings 'for of', 'for each' and 'for in'.

--- Loops/Loops.js
+++ Loops/Loops.js
@@ -28,3 +28,42 @@ for (let i = 0; i <= notas.length; i++) {
 }
 
 console.log(total);
+
+
+//outro exemplo:
+
+const carros = ["Mercedes", "Toyota", "Ferrari"];
+
+for (let i; i < carros.length; i++){
+    console.log(carros[i])
+}// executa enquanto o "i" for menor que a quantidade de itens do array.
+
+//faz o mesmo que o for de cima:
+for (let car of carros){
+    console.log(car);
+}
+
+
+// for each
+
+carros.forEach(function(car, i){
+    console.log(i)
+    console.log(car);
+});
+//faz um looping com função que da o mesmo resultado dos dois últimos for.
+
+// for in
+
+const pessoa = {
+    nome: "andre",
+    idade: 21,
+};
+
+//tem mais serventia para objetos, onde ele acessa o objeto e executa nas propriedades.
+
+for (propriedade in pessoa){
+    console.log(property)
+    console.log(pessoa[propriedade]);
+}
+
+//não é muito perfomatico, por isso muitos devs usam apenas em casos especificos.
