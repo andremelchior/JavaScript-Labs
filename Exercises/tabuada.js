20787a4210bd69e3dc1c91e2d20d16890db9f31c
André Melchior <andre.melchior@outlook.com>
2025-02-13 23:57:09 -0300

    feat: add multiplication table algorithm

---
+++ Exercises/tabuada.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 11; i++) {
+    let n1 = i
+    console.log("\nTabuada do " + n1)
+
+    for (let count = 0; count < 11; count++) {
+        console.log(n1 + "x" + count + " = " + n1 * count);
+    }
+}
